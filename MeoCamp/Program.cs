84fe395e5f54b7cafad722f467e2babce267900f
SE173396_LeNhatHuy <huylnse173396@fpt.edu.vn>
2024-09-20 17:37:11 +0700

    enable cors

--- MeoCamp/Program.cs
+++ MeoCamp/Program.cs
@@ -13,10 +13,19 @@ namespace MeoCamp
     {
         public static void Main(string[] args)
         {   
+            
 
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<MeoCampDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder => builder.AllowAnyOrigin()   // Allow any origin
+                                      .AllowAnyMethod()   // Allow any HTTP method
+                                      .AllowAnyHeader()); // Allow any headers
+            });
 
             // Add services to the container.
             builder.Services.AddScoped<GenericRepository<Product>>();
@@ -25,7 +34,8 @@ namespace MeoCamp
             builder.Services.AddScoped<ProductRepository>();
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddControllers();
-            
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -40,6 +50,9 @@ namespace MeoCamp
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("AllowAll");  // Apply the CORS policy defined above
+
             app.UseAuthentication();
             app.UseAuthorization();
 
